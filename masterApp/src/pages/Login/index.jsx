159305c951ec9f50410a30792ea31321e2c00757
guojunwei <840126489@qq.com>
2020-05-13 00:38:13 +0800

    fix: 修复form未定义错误

--- masterApp/src/pages/Login/index.jsx
+++ masterApp/src/pages/Login/index.jsx
@@ -1,7 +1,6 @@
 import { FieldForm } from '@/components';
 import { Form, Button, Row, Col, Icon, Radio, Input, message } from 'antd';
 import LoginLayout from '@/layouts/LoginLayout';
-import './index.module.less';
 
 const formItemLayout = {
     labelCol: {
@@ -12,60 +11,79 @@ const formItemLayout = {
     }
 }
 
-class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.fields = [
-            { type: 'input', id: 'username', className: 'input-form', value: '', prefix: <Icon type='user' />, placeholder: '请输入用户名' },
-            { type: 'password', id: 'password', className: 'input-form', value: '', col: 24, prefix: <Icon type="lock" />, placeholder: '请输入密码' },
-        ]
-    }
+function Login(props) {
+    const { form, history } = props;
+    const fields = [
+        { type: 'input', id: 'username', className: 'input-form', value: '', prefix: <Icon type='user' />, placeholder: '请输入用户名' },
+        { type: 'password', id: 'password', className: 'input-form', value: '', col: 24, prefix: <Icon type="lock" />, placeholder: '请输入密码' },
+    ]
 
-    handleSubmit = () => {
-        this.props.form.validateFieldsAndScroll((err, values) => {
+    /**
+     * @description 处理提交
+     */
+    function handleSubmit() {
+
+        form.validateFieldsAndScroll((err, values) => {
             if (err) { return; }
             if (values.username === 'admin' && values.password === '123456') {
-                this.props.history.push('/system');
+                history.push('/system');
             } else {
                 message.error('用户名或密码错误');
             }
         })
     }
 
-    handleKeyDown = (e) => {
-        console.log(e.keyCode);
-        if (e.keyCode === 13) {
-            this.handleSubmit();
-        }
-    }
-
-    render() {
-        return (
-            <LoginLayout>
-                <Form className='login-wrap' onSubmit={this.handleSubmit}>
-                    <h2 className="login-title">后台管理系统</h2>
-                    <div className="input-wrap-form">
-                        {
-                            this.fields.map(item => {
-                                const { col, ...rest } = item;
-                                return (
-                                    <Col span={item.col} key={item.id}>
-                                        <FieldForm {...rest} form={this.props.form} />
-                                    </Col>
-                                )
-                            })
-                        }
-                        <Button
-                            type='primary' style={{ width: '100%' }}
-                            htmlType='submit'
-                            onClick={this.handleSubmit}
-                        >登录</Button>
-                    </div>
-                </Form>
-            </LoginLayout>
-        )
-
-    }
+    return (
+        <LoginLayout>
+            <LoginFormWrap onSubmit={handleSubmit}>
+                <LoginTitle>后台管理系统</LoginTitle>
+                <InputWrapForm>
+                    {
+                        fields.map(item => {
+                            const { col, ...rest } = item;
+                            return (
+                                <Col span={item.col} key={item.id}>
+                                    <FieldForm {...rest} form={form} />
+                                </Col>
+                            )
+                        })
+                    }
+                    <Button
+                        type='primary'
+                        style={{ width: '100%' }}
+                        htmlType='submit'
+                        onClick={handleSubmit}
+                    >
+                        登录</Button>
+                </InputWrapForm>
+            </LoginFormWrap>
+        </LoginLayout>
+    )
 }
+
+const LoginFormWrap = styled(Form)`
+    position        : absolute;
+    top             : 50%;
+    right           : 20%;
+    width           : 420px;
+    height          : 500px;
+    margin-top      : -250px;
+    border          : 1px solid white;
+    border-radius   : 10px;
+    background-color: rgba(255, 255, 255, .7);
+    box-shadow      : -15px 15px 15px rgba(6, 17, 47, .7);
+`
+
+const LoginTitle = styled.h2`
+    text-align : center;
+    line-height: 2;
+    font-size  : 26px;
+    margin     : 100px auto 20px;
+`
+
+const InputWrapForm = styled.div`
+    max-width: 300px;
+    margin   : 0 auto 20px;
+`
 
 export default Form.create()(Login);
